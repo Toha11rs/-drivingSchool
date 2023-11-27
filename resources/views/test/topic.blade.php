2f2c06ca9a10c458ac0cca005a34f81027053a5c
Toha11rs <son07027q@gmail.com>
2023-11-27 23:16:07 +0300

    -ignor

--- resources/views/test/topic.blade.php
+++ resources/views/test/topic.blade.php
@@ -8,12 +8,14 @@
     @endphp
 @endsection
 @section('content')
-<div id="quiz">
+
+<div id="progress"></div>
+<div id="quiz pdd-ticket" style="background: #f5f5f5;">
+    <p id="counter"></p>
     <h2 id="question"></h2>
     <ul id="options"></ul>
     <p id="result"></p>
-    <p id="counter"></p>
-    <div id="progress"></div>
+
     <button id="answer-btn" style="display: none;">Ответить</button>
     <button id="next-btn" style="display: none;">Следующий вопрос</button>
     <div id="answer-tip"></div>
