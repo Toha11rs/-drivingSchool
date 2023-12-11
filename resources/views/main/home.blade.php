1d7cf89d289f8efc7f6a959bb880570806486ce5
Toha11rs <son07027q@gmail.com>
2023-12-11 04:19:20 +0300

    фронт панели клиента

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -10,6 +10,10 @@
   <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/headers/">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+<link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" media="all">
+<link href="{{ asset('css/hamburgers.min.css') }}" rel="stylesheet" media="all">
+<link href="{{ asset('css/profile.css') }}" rel="stylesheet" media="all">
+<link href="{{ asset('css/main.css') }}" rel="stylesheet" media="all">
 
 <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -26,27 +30,165 @@
 });
 </script>
 
+<header class="header-desktop3 d-none d-lg-block">
+  <div class="section__content section__content--p35">
+      <div class="header3-wrap">
+          <div class="header__logo">
+              <a href="#">
+                  <img src="{{ asset('images/logo.png') }}" width="150" alt="Автошкола Drive" />
+              </a>
+          </div>
+          <div class="header__navbar">
+              <ul class="list-unstyled">
+                  <li class="has-sub">
+                      <a href="#">
+                          <span class="bot-line"></span>
+                          Главная
+                      </a>
+                  </li>
+                  <li>
+                      <a href="#">
+                          
+                          <span class="bot-line"></span>О нас</a>
+                  </li>
+                  <li>
+                      <a href="table.html">
+                          
+                          <span class="bot-line"></span>Контакты</a>
+                  </li>
+                  <li class="has-sub">
+                      <a href="#">
+                          <span class="bot-line"></span>Услуги и цены</a>   
+                  </li>
+                  <li class="has-sub">
+                    <a href="#">
+                        <span class="bot-line"></span>FAQ</a>   
+                </li>
+                
+              </ul>
+          </div>
+          <div class="header__tool">
+              <div class="account-wrap">
+                  <div class="account-item account-item--style2 clearfix js-item-menu">
+                      <div class="content">
+                          <a class="js-acc-btn" href="#">Имя Фамилия </a>
+                      </div>
+                      <div class="account-dropdown js-dropdown">
+                          <div class="info clearfix">
+                              
+                              <div class="content">
+                                  <h5 class="name">
+                                      <a href="#">Имя Фамилия</a>
+                                  </h5>
+                                 
+                              </div>
+                          </div>
+                          <div class="account-dropdown__body">
+                            <div class="account-dropdown__item">
+                              <a href="#">
+                                  <i class="zmdi zmdi-account"></i>Обучение</a>
+                          </div>
+                              <div class="account-dropdown__item">
+                                  <a href="#">
+                                      <i class="zmdi zmdi-account"></i>Аккаунт</a>
+                              </div>
+                              
+                              <div class="account-dropdown__item">
+                                  <a href="#">
+                                      <i class="zmdi zmdi-money-box"></i>Настройки</a>
+                              </div>
+                          </div>
+                          <div class="account-dropdown__footer">
+                              <a href="#">
+                                  <i class="zmdi zmdi-power"></i>Выйти</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</header>
+<header class="header-mobile header-mobile-2 d-block d-lg-none">
+  <div class="header-mobile__bar">
+      <div class="container-fluid">
+          <div class="header-mobile-inner">
+              <a class="logo" href="index.html">
+                  <img src="{{ asset('images/logo.png') }}" width="150" alt="Автошкола Drive" />
+              </a>
+              <div class="account-wrap">
+                  <div class="account-item account-item--style2 clearfix js-item-menu">
+                      
+                      <div class="content">
+                          <a class="js-acc-btn" href="#">Имя Фамилия</a>
+                      </div>
+                      <div class="account-dropdown js-dropdown">
+                          <div class="info clearfix">
+                              
+                              <div class="content">
+                                  <h5 class="name">
+                                      <a href="#">Имя Фамилия</a>
+                                  </h5> 
+                              </div>
+                          </div>
+                          <div class="account-dropdown__body">
+                            <div class="account-dropdown__item">
+                              <a href="#">
+                                  <i class="zmdi zmdi-account"></i>Обучение</a>
+                          </div>
+                              <div class="account-dropdown__item">
+                                  <a href="#">
+                                      <i class="zmdi zmdi-account"></i>Аккаунт</a>
+                              </div>
+                              <div class="account-dropdown__item">
+                                  <a href="#">
+                                      <i class="zmdi zmdi-settings"></i>Настройки</a>
+                              </div>
+                          </div>
+                          <div class="account-dropdown__footer">
+                              <a href="#">
+                                  <i class="zmdi zmdi-power"></i>Выйти</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <button class="hamburger hamburger--slider" type="button">
+                  <span class="hamburger-box">
+                      <span class="hamburger-inner"></span>
+                  </span>
+              </button>
+          </div>
+      </div>
+  </div>
+  <nav class="navbar-mobile">
+      <div class="container-fluid">
+          <ul class="navbar-mobile__list list-unstyled">
+              <li class="has-sub">
+                  <a class="js-arrow" href="#">
+                      <i class="fas fa-tachometer-alt"></i>Главная</a>
+              </li>
+              <li>
+                  <a href="chart.html">
+                      <i class="fas fa-chart-bar"></i>О нас</a>
+              </li>
+              <li>
+                  <a href="table.html">
+                      <i class="fas fa-table"></i>Контакты</a>
+              </li>
+              <li>
+                  <a href="form.html">
+                      <i class="far fa-check-square"></i>Услуги и цены</a>
+              </li>
+              <li>
+                <a href="form.html">
+                    <i class="far fa-check-square"></i>FAQ</a>
+            </li>
+          </ul>
+      </div>
+  </nav>
+</header>
+
 <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3">
-      <div class="col-md-3 mb-2 mb-md-0">
-        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
-          <img src="{{ asset('images/logo.png') }}"  width="200"  ><use xlink:href="#bootstrap"/></img>
-        </a>
-      </div>
-
-      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link1 px-2 headerssize link-secondary">Главная</a></li>
-        <li><a href="#" class="nav-link1 px-2 headerssize">О нас</a></li>
-        <li><a href="#" class="nav-link1 px-2 headerssize">Контакты</a></li>
-        <li><a href="#" class="nav-link1 px-2 headerssize">Услуги и цены</a></li>
-        <li><a href="#" class="nav-link1 px-2 headerssize">FAQ</a></li>
-      </ul>
-
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn1 btn-outline-primary1 me-2">Начать учиться</button>
-        <button type="button" class="btn1 color">Войти</button>
-      </div>
-    </header>
   </div>
 
 <body style="margin: 0; padding: 0;overflow-x: hidden;">
@@ -441,6 +583,15 @@
 </div>
 
 </body>
+
+<script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+   
+ 
+   
+<script src="{{ asset('css/animsition.min.js') }}"></script>
+
+<!-- Main JS-->
+<script src="{{ asset('js/main.js') }}"></script>
 
 
 @endsection
